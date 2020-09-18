f9c88cf35b001752b6116e2ac0e7450475518acc
jaybur1 <jayburbyga@gmail.com>
2020-09-18 17:06:41 -0400

    nav bar modifications + add js script to handle nav items

---
+++ static/js/script.js
@@ -0,0 +1,23 @@
+const unActiveNavItems = () => 
+        document.querySelectorAll('.navbar-link').forEach(el => el.classList.remove('active'));
+
+const onHomeClick = (e) => {
+    if(window.location.pathname === '/'){
+        unActiveNavItems()
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+          });
+    }else{
+        const home = document.querySelector('.home-btn')
+        home.href = '/'
+        home.click()
+    }
+    document.querySelector('.home-btn').classList.add('active')
+}
+
+const onNavItemClick = (item) => {
+    unActiveNavItems()
+    document.querySelector(item).classList.add('active')
+}
