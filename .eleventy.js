d455e996f0dd6882638f3bad9581a2304849953b
jaybur1 <jayburbyga@gmail.com>
2020-10-07 10:27:44 -0400

    add mobile responsivnes

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,11 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('static');
     eleventyConfig.addPassthroughCopy('admin');
     return {
-        passthroughFileCopy: true
+        passthroughFileCopy: true,
+        dataTemplateEngine: "ejs",
+        markdownTemplateEngine: "ejs",
+        htmlTemplateEngine: "ejs",
+        templateFormats: ["md", "html", "png", "jpg", "jpeg", "gif", "svg"],
     };
     
 };
